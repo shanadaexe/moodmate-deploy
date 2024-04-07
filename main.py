36499106213ca0c6f0c9a314951374736e0a08ba
Shanada.exe <79242171+shanadaexe@users.noreply.github.com>
2024-04-07 23:29:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,8 @@ async def predict_Emotoin(filepath: str):
     try:
         print(f"Filepath: {filepath}")
 
-        input_file_path = '/Users/isurudissanayake/DataspellProjects/FYP_Implementation/aunite/src/CaptureImages/' + filepath
+        # input_file_path = '/Users/isurudissanayake/DataspellProjects/FYP_Implementation/aunite/src/CaptureImages/' + filepath
+        input_file_path = filepath
         image_path = input_file_path
 
         emotion, probability = predictEmotion(image_path)
@@ -72,46 +73,46 @@ async def predict_Emotoin(filepath: str):
 
 
 
-@app.post("/save_image")
-async def save_image(image: UploadFile = File(...)):
-    if not image.content_type.startswith('image'):
-        raise HTTPException(status_code=415, detail="Unsupported Media Type. Please provide an image file.")
+# @app.post("/save_image")
+# async def save_image(image: UploadFile = File(...)):
+#     if not image.content_type.startswith('image'):
+#         raise HTTPException(status_code=415, detail="Unsupported Media Type. Please provide an image file.")
+#
+#     save_folder = '/Users/isurudissanayake/DataspellProjects/FYP_Implementation/aunite/src/CaptureImages'  # Update with your desired save folder path
+#     if not os.path.exists(save_folder):
+#         os.makedirs(save_folder)
+#
+#     # Generate a unique filename using UUID
+#     unique_filename = str(uuid4()) + '.jpg'
+#     image_path = os.path.join(save_folder, unique_filename)
+#
+#     with open(image_path, "wb") as image_file:
+#         content = await image.read()
+#         image_file.write(content)
+#     print(f"Filename: {unique_filename}");
+#     return {"message": "Image saved successfully", "filename": unique_filename}
 
-    save_folder = '/Users/isurudissanayake/DataspellProjects/FYP_Implementation/aunite/src/CaptureImages'  # Update with your desired save folder path
-    if not os.path.exists(save_folder):
-        os.makedirs(save_folder)
 
-    # Generate a unique filename using UUID
-    unique_filename = str(uuid4()) + '.jpg'
-    image_path = os.path.join(save_folder, unique_filename)
-
-    with open(image_path, "wb") as image_file:
-        content = await image.read()
-        image_file.write(content)
-    print(f"Filename: {unique_filename}");
-    return {"message": "Image saved successfully", "filename": unique_filename}
-
-
-@app.post("/upload_image")
-async def upload_image(image: UploadFile = File(...)):
-    try:
-        if not image.content_type.startswith('image'):
-            raise HTTPException(status_code=415, detail="Unsupported Media Type. Please provide an image file.")
-
-        save_folder = '/Users/isurudissanayake/DataspellProjects/FYP_Implementation/aunite/src/CaptureImages'  # Update with your desired save folder path
-        if not os.path.exists(save_folder):
-            os.makedirs(save_folder)
-
-        # Generate a unique filename using UUID
-        unique_filename = str(uuid4()) + '.jpg'
-        image_path = os.path.join(save_folder, unique_filename)
-
-        with open(image_path, "wb") as image_file:
-            content = await image.read()
-            image_file.write(content)
-
-        print(f"Filename: {unique_filename}")
-        return {"message": "Image saved successfully", "filename": unique_filename}
-
-    except Exception as e:
-        return {"error": f"Failed to upload image: {str(e)}"}
+# @app.post("/upload_image")
+# async def upload_image(image: UploadFile = File(...)):
+#     try:
+#         if not image.content_type.startswith('image'):
+#             raise HTTPException(status_code=415, detail="Unsupported Media Type. Please provide an image file.")
+#
+#         save_folder = '/Users/isurudissanayake/DataspellProjects/FYP_Implementation/aunite/src/CaptureImages'  # Update with your desired save folder path
+#         if not os.path.exists(save_folder):
+#             os.makedirs(save_folder)
+#
+#         # Generate a unique filename using UUID
+#         unique_filename = str(uuid4()) + '.jpg'
+#         image_path = os.path.join(save_folder, unique_filename)
+#
+#         with open(image_path, "wb") as image_file:
+#             content = await image.read()
+#             image_file.write(content)
+#
+#         print(f"Filename: {unique_filename}")
+#         return {"message": "Image saved successfully", "filename": unique_filename}
+#
+#     except Exception as e:
+#         return {"error": f"Failed to upload image: {str(e)}"}
